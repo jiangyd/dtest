24f07948b4fedf1c2c2feb7fc72d6b83284d2b7d
jiangyd <962584902@qq.com>
2021-06-10 18:00:10 +0800

    更换目录

--- dtest/run.py
+++ dtest/run.py
@@ -3,6 +3,7 @@ import os
 import sys
 import asyncio
 
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dtest.suite import parseSuite
 from dtest.util import loadSuiteFiles
